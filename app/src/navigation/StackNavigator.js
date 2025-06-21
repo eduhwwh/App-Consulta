253eedbff50aea3412cca08b9b68195fa27cadc3
Maria Eduarda <vantev806@gmail.com>
2025-06-21 12:11:18 -0500

    Criação do Projeto

---
+++ app/src/navigation/StackNavigator.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function StackNavigator(){
+    return(
+        <StackNavigator initialRouteName = 'Login'>
+            <Stack.Screen name = 'Login' component = {LoginScreen}/>
+            <Stack.Screen name = 'Home' component = {HomeScreen}/>
+        </StackNavigator>
+    );
+}
+
+
